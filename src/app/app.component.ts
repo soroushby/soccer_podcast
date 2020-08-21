02355fd47860508f82276763004e4e1d72cca0e1
soroushby <49848393+soroushby@users.noreply.github.com>
2020-08-21 00:52:42 -0700

    login works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { AuthService } from './services/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'soccerPodcast';
+
+  constructor(public auth: AuthService) {}
 }
